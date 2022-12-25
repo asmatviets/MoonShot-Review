9524d69e816a5fb904ed4e01bfb7e0717d90b7cf
AndreyMatviets <asmatviets@gmail.com>
2022-12-25 08:23:25 +0400

    Initial Commit

---
+++ MoonShot-Revies/MoonShot_ReviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShot_ReviesApp.swift
+//  MoonShot-Revies
+//
+//  Created by Andrey Matviets on 25.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonShot_ReviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
